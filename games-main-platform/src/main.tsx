813187386f007e62e5e3254552874e91d6d8edbb
AndrasCsorba <andreascs@mail.ch>
2025-04-14 11:32:11 +0200

    clear

--- games-main-platform/src/main.tsx
+++ games-main-platform/src/main.tsx
@@ -1,13 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ChakraProvider value={undefined} children={undefined}>
-      <App />
-    </ChakraProvider>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
